2e8a92e694ba724d7a2553f49a71893d806d27ae
Will McLendon <wimclend@gmail.com>
2019-11-08 17:08:26 -0500

    some test cleanup

--- tests/test_devicedata_security.py
+++ tests/test_devicedata_security.py
@@ -58,17 +58,6 @@ class Test_FortigateFirewallPolicy:
         ]
         for col in expected_columns:
             assert col in data.columns
-        # assert "policyid" in data.columns
-        # assert "name" in data.columns
-        # assert "logtraffic" in data.columns
-        # assert "application" in data.columns
-        # assert "action" in data.columns
-        # assert "service" in data.columns
-        # assert "dstaddr" in data.columns
-        # assert "dstintf" in data.columns
-        # assert "srcaddr" in data.columns
-        # assert "srcintf" in data.columns
-        # assert "comments" in data.columns
         assert data["name"].str.contains(example_name).any()
         numpy.testing.assert_array_equal(data["srcaddr"][1], example_srcaddr)
         numpy.testing.assert_array_equal(data["dstaddr"][1], example_dstaddr)
